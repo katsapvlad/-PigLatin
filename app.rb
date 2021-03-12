ce41c722dc5619ecaeb09d8d7215855b4dde3f65
Vlad Katsap <70981345+katsapvlad@users.noreply.github.com>
2021-03-12 12:34:10 +0300

    Add tests and translator for one-word translate

---
+++ app.rb
@@ -0,0 +1,34 @@
+require "rubygems"
+require "minitest/autorun"
+
+class PiglatinTranslatorTest < MiniTest::Test
+  ARR = ['a','e','i','o','u']
+  def test_pig_becomes_igpay
+    assert_equal "igpay", translate("pig")
+  end
+
+  def test_game_becomes_amegay
+    assert_equal "amegay", translate("game")
+  end
+
+  def test_age_becomes_ageyay
+    assert_equal "ageyay", translate("age")
+  end
+
+  def test_swap_becomes_apsway
+    assert_equal "apsway", translate("swap")
+  end
+
+  def test_strange_becomes_angestray
+    assert_equal "angestray", translate("strange")
+  end
+
+  def translate(word)
+    if ARR.include?(word[0])
+      "#{word + 'yay'}"
+    else
+      i = word.chars.find_index {|item| ARR.include?(item)}
+      "#{word[i..-1] + word[0...i] + 'ay'}" 
+    end
+  end
+end
